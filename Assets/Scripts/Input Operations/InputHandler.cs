d798375e2f2957c9a24af640b25dcad7be5432ee
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-05-30 18:14:51 +0300

    Tooltips and Clean Code Refactoring

--- Assets/Scripts/Input Operations/InputHandler.cs
+++ Assets/Scripts/Input Operations/InputHandler.cs
@@ -1,42 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿/// Author: Yağız A. AYER
+/// Github: github.com/yagizayer
+/// Date: 30 May 2021
+/// Used Style guide: Google C# StyleGuide (https://google.github.io/styleguide/csharp-style.html)
+
+
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
+
+// creating templates for required events
 [Serializable]
 public class MoveInputEvent : UnityEvent<float, float> { }
 [Serializable]
 public class SprintInputEvent : UnityEvent<bool> { }
+
+
 public class InputHandler : MonoBehaviour
 {
-    public MoveInputEvent moveInputEvent;
-    public SprintInputEvent sprintInputEvent;
-    InputManager inputManager;
+
+    [Tooltip("Player character event for forward, backward, left, right movement")]
+    [SerializeField] private MoveInputEvent _moveInputEvent;
+    [Tooltip("Player character event for sprint movement")]
+    [SerializeField] private SprintInputEvent _sprintInputEvent;
+    private InputManager _inputManager;
 
     private void Awake()
     {
-        inputManager = new InputManager();
+        _inputManager = new InputManager();
     }
     private void OnEnable()
     {
-        inputManager.Gameplay.Enable();
-        inputManager.Gameplay.Move.performed += OnMovePerformed;
-        inputManager.Gameplay.Move.canceled += OnMovePerformed;
-        inputManager.Gameplay.Sprint.started += OnSprintStarted;
-        inputManager.Gameplay.Sprint.canceled += OnSprintStarted;
+        _inputManager.Gameplay.Enable();
+        _inputManager.Gameplay.Move.performed += OnMovePerformed;
+        _inputManager.Gameplay.Move.canceled += OnMovePerformed;    // we are binding on cancelled too because we want to 
+                                                                    // perform one more movement(stopping move) when player relase the key
+        _inputManager.Gameplay.Sprint.started += OnSprintStarted;
+        _inputManager.Gameplay.Sprint.canceled += OnSprintStarted;  // we are binding on cancelled too because we want to 
+                                                                    // perform one more movement(stopping move) when player relase the key
     }
     void OnMovePerformed(InputAction.CallbackContext context)
     {
         Vector3 moveInput = context.ReadValue<Vector2>();
-        moveInputEvent.Invoke(moveInput.x, moveInput.y);
+        _moveInputEvent.Invoke(moveInput.x, moveInput.y);
     }
     void OnSprintStarted(InputAction.CallbackContext context)
     {
         if (context.started)
-            sprintInputEvent.Invoke(true);
+            _sprintInputEvent.Invoke(true);
         if (context.canceled)
-            sprintInputEvent.Invoke(false);
+            _sprintInputEvent.Invoke(false);
     }
 }
--- Assets/Scripts/Player Operations/ComplexMove.cs
+++ Assets/Scripts/Player Operations/ComplexMove.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/// Author: Yağız A. AYER
+/// Github: github.com/yagizayer
+/// Date: 30 May 2021
+/// Used Style guide: Google C# StyleGuide (https://google.github.io/styleguide/csharp-style.html)
+
+
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class ComplexMove : MonoBehaviour
 {
 
-    public CharacterController controller;
-    public Transform currentCamera;
-    public Animator animator;
+    [SerializeField] private float _moveSpeed = 12f, _rotationSpeed = 720;
 
-
-
-    public float moveSpeed = 12f, rotationSpeed = 200;
-    bool isGrounded, moving = false, isSprinting = false;
-
-
-    float horizontal = 0, vertical = 0;
-    Vector3 gravityForce;
+    private CharacterController controller;
+    private Transform currentCamera;
+    private Animator animator;
+    private bool _isGrounded, _moving = false, _isSprinting = false;
+    private float _horizontal = 0, _vertical = 0;
+    private Vector3 _gravityForce;
 
     private void Start()
     {
@@ -27,49 +28,51 @@ public class ComplexMove : MonoBehaviour
     void Update()
     {
         MoveCharacter();
+        // as we dont have any rigidbody component(character controller doesnt allow) we need to make our own gravity force
         MakeGravity();
     }
 
-    void MoveCharacter(){
-        Vector3 rawMoveDir = Vector3.forward * vertical + Vector3.right * horizontal;
+    void MoveCharacter()
+    {
+        Vector3 rawMoveDir = Vector3.forward * _vertical + Vector3.right * _horizontal;
 
         Vector3 cameraForwardNormalized = Vector3.ProjectOnPlane(currentCamera.forward, Vector3.up);
         Quaternion rotationToCamNormal = Quaternion.LookRotation(cameraForwardNormalized, Vector3.up);
 
         Vector3 finalMoveDir = rotationToCamNormal * rawMoveDir;
 
-        moving = false;
+        _moving = false;
         if (!Vector3.Equals(finalMoveDir, Vector3.zero))
         {
             Quaternion rotationToMoveDir = Quaternion.LookRotation(finalMoveDir, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMoveDir, rotationSpeed * Time.deltaTime);
-            moving = true;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMoveDir, _rotationSpeed * Time.deltaTime);
+            _moving = true;
         }
 
-        animator.SetBool("Running", moving);
-        animator.SetBool("Sprinting", isSprinting);
+        animator.SetBool("Running", _moving);
+        animator.SetBool("Sprinting", _isSprinting);
 
 
-        controller.Move(finalMoveDir * (isSprinting ? moveSpeed * 2 : moveSpeed) * Time.deltaTime);
+        controller.Move(finalMoveDir * (_isSprinting ? _moveSpeed * 2 : _moveSpeed) * Time.deltaTime);
     }
     void MakeGravity()
     {
-        isGrounded = Physics.CheckSphere(transform.position, .5f);
-        if (isGrounded && gravityForce.y < 0)
+        _isGrounded = Physics.CheckSphere(transform.position, .5f);
+        if (_isGrounded && _gravityForce.y < 0)
         {
-            gravityForce.y = -2f;
+            _gravityForce.y = -2f;
         }
-        gravityForce.y += -9.8f * Time.deltaTime * Time.deltaTime;
-        controller.Move(gravityForce);
+        _gravityForce.y += -9.8f * Time.deltaTime * Time.deltaTime;
+        controller.Move(_gravityForce);
     }
 
     public void OnMoveInput(float horizontal, float vertical)
     {
-        this.horizontal = horizontal;
-        this.vertical = vertical;
+        this._horizontal = horizontal;
+        this._vertical = vertical;
     }
     public void OnSprintInput(bool sprintInput)
     {
-        this.isSprinting = sprintInput;
+        this._isSprinting = sprintInput;
     }
 }
